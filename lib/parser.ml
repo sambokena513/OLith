6acf2860889a4f70a539f85a2a59ee38d3db491a
Ofenbauer <sambokena@mail.com>
2026-03-06 20:37:40 +0100

    Fix nested lists in parser.

--- lib/parser.ml
+++ lib/parser.ml
@@ -17,52 +17,45 @@ type atom =
   | AChar of char
 
 type sexpr =
-  | SAtom of atom
-  | SList of sexpr list
+  | SAtom of Lexer.pos_info * atom
+  | SList of Lexer.pos_info * sexpr list
 
 type error =
-  | UnclosedList
-  | UnmatchedRPAREN
+  | UnclosedList of Lexer.pos_info
+  | UnmatchedRPAREN of Lexer.pos_info
 
 let rec parse
   (tokens : (Lexer.pos_info * Lexer.token) list)
   (stck : (Lexer.pos_info * sexpr list) list)
-  (acc : (Lexer.pos_info * (sexpr, error) result) list)
+  (acc : ((sexpr, error) result) list)
   =
   let open Lexer in
   let update_pos st en = {en with start = st.start} in
-  let lift_st (a, _) = (a, Error UnclosedList) in
   match tokens with
   | [] -> (match stck with
             | [] -> List.rev acc
-            | stck -> List.rev_append (List.map lift_st stck) acc 
+            | stck -> List.rev_append (List.map (fun (p, _) -> Error (UnclosedList p)) stck) acc 
           )
   | (pos, tok) :: rest -> (
-    match stck with
-    | [] -> (
-      match tok with
-      | TInt x -> parse rest stck ((pos, Ok (SAtom (AInt x))) :: acc)
-      | TFloat x -> parse rest stck ((pos, Ok (SAtom (AFloat x))) :: acc)
-      | TSymbol x -> parse rest stck ((pos, Ok (SAtom (ASymbol x))) :: acc)
-      | TString x -> parse rest stck ((pos, Ok (SAtom (AString x))) :: acc)
-      | TChar x -> parse rest stck ((pos, Ok (SAtom (AChar x))) :: acc)
-      | QUASIQUOTE -> parse rest stck ((pos, Ok (SAtom (ASymbol "quasiquote"))) :: acc)
-      | UNQUOTE -> parse rest stck ((pos, Ok (SAtom (ASymbol "unquote"))) :: acc)
-      | UNQUOTE_SPLICE -> parse rest stck ((pos, Ok (SAtom (ASymbol "unquote_splice"))) :: acc)
-      | TLPAREN -> parse rest ((pos, []) :: stck) acc
-      | TRPAREN -> parse rest stck ((pos, Error UnmatchedRPAREN) :: acc)
-    )
-    | (ps, top) :: stck -> (
-      match tok with
-      | TInt x -> parse rest (((update_pos ps pos), (SAtom (AInt x)) :: top) :: stck) acc
-      | TFloat x -> parse rest (((update_pos ps pos), (SAtom (AFloat x)) :: top) :: stck) acc
-      | TSymbol x -> parse rest (((update_pos ps pos), (SAtom (ASymbol x)) :: top) :: stck) acc
-      | TString x -> parse rest (((update_pos ps pos), (SAtom (AString x)) :: top) :: stck) acc
-      | TChar x -> parse rest (((update_pos ps pos), (SAtom (AChar x)) :: top) :: stck) acc
-      | QUASIQUOTE -> parse rest (((update_pos ps pos), (SAtom (ASymbol "quasiquote")) :: top) :: stck) acc
-      | UNQUOTE -> parse rest (((update_pos ps pos), (SAtom (ASymbol "unquote")) :: top) :: stck) acc
-      | UNQUOTE_SPLICE -> parse rest (((update_pos ps pos), (SAtom (ASymbol "unquote_splice")) :: top) :: stck) acc
-      | TLPAREN -> parse rest (((update_pos ps pos), []) :: stck) acc
-      | TRPAREN -> parse rest stck ((update_pos ps pos, Ok (SList (List.rev top))) :: acc)
+    let push_atom atom =
+      match stck with
+      | [] -> parse rest stck (Ok (SAtom (pos, atom)) :: acc)
+      | (ps, top) :: stck -> parse rest (((update_pos ps pos), (SAtom (pos, atom)) :: top) :: stck) acc
+    in
+    match tok with
+    | TInt x -> push_atom (AInt x)
+    | TFloat x -> push_atom (AFloat x)
+    | TSymbol x -> push_atom (ASymbol x)
+    | TString x -> push_atom (AString x)
+    | TChar x -> push_atom (AChar x)
+    | QUASIQUOTE -> push_atom (ASymbol "quasiquote")
+    | UNQUOTE -> push_atom (ASymbol "unquote")
+    | UNQUOTE_SPLICE -> push_atom (ASymbol "unquote_splice")
+    | TLPAREN -> parse rest ((pos, []) :: stck) acc
+    | TRPAREN -> (
+      match stck with
+      | [] -> parse rest [] (Error (UnmatchedRPAREN pos) :: acc)
+      | (ps, top) :: (pps, ptop) :: stck -> parse rest ((update_pos pps pos, SList (update_pos ps pos, List.rev top) :: ptop) :: stck) acc
+      | (ps, top) :: [] -> parse rest [] (Ok (SList (update_pos ps pos, List.rev top)) :: acc)
     )
   )
